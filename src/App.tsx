946a06473c2e058c4471d9af64b3f7d7da759e06
volkovigor <info@viil.ru>
2023-11-28 04:28:18 +0500

    Логика ввода слов с клавитуры + Добавлено API Яндекс Словаря для проверки слов + Составлен словарь слов из 5 букв + Функция-рандомайзер слова для начала игры

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,50 @@
+import {useEffect, useMemo, useState} from "react"
+import {wordsData} from './words.ts'
+
+const WORD = 'Жесть'
+
 function App() {
+  const [word, setWord] = useState<string>('')
+  // const [attempt, setAttempt] = useState<string[]>(new Array(6).fill(null))
+
+  const randomWord = useMemo(() => {
+    if(!wordsData.length) return
+
+    return wordsData[Math.floor(Math.random() * wordsData.length - 1)]
+  },[])
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (word.length && event.key === 'Backspace') {
+        setWord(word => word.slice(0, word.length - 1))
+      }
+
+      if (word.length === 5 && event.key === 'Enter') {
+        console.log('Enter')
+      }
+
+      if (word.length >= 5 || event.key.match(/[^а-яА-Я]+/g)) return
+      setWord(word => word + event.key)
+    }
+
+    document.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [word.length])
+
+  useEffect(() => {
+    fetch(`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=
+    ${import.meta.env.VITE_API_KEY}&lang=ru-ru&text=${WORD}`)
+      .then(resolve => resolve.json())
+      .then(data => console.log(data, 'API Словаря Яндекс', WORD))
+  }, [])
 
   return (
     <>
-      Здесь будет игра
+      <div>Слово: {word}</div>
+      {randomWord}
     </>
   )
 }
